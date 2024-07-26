57db965e593d1a24e799031e61815a7227feccb3
fuirian <kmjkhj2017@gmail.com>
2024-07-26 11:00:27 +0900

    refactor: diary title content

--- mju/src/main/java/todo/mju/domain/diary/controller/dto/response/DiaryAllResponse.java
+++ mju/src/main/java/todo/mju/domain/diary/controller/dto/response/DiaryAllResponse.java
@@ -7,18 +7,15 @@ import java.util.List;
 
 @Getter
 public class DiaryAllResponse {
-    private final int diaryCount;
     private final List<DiaryDto> diaryDtos;
 
     @Builder
-    public DiaryAllResponse(final int diaryCount, final List<DiaryDto> diaryDtos) {
-        this.diaryCount = diaryCount;
+    public DiaryAllResponse(final List<DiaryDto> diaryDtos) {
         this.diaryDtos = diaryDtos;
     }
 
     public static DiaryAllResponse of(List<DiaryDto> diaryDtos) {
         return DiaryAllResponse.builder()
-                .diaryCount(diaryDtos.size())
                 .diaryDtos(diaryDtos)
                 .build();
     }
